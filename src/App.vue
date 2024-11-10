56d5a5153159cdd20c50a9400c3aa68e56c23b11
Paulo Silva <pauloecomercial367@gmail.com>
2024-11-10 20:55:47 -0300

    :gem: login page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,70 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="login-area">
+            <form>
+              <div class="form-group">
+                <label>E-mail: </label>
+                <input type="text" class="form-control" />
+              </div>
+              <div class="form-group">
+                <label>Senha: </label>
+                <input type="password" class="form-control" />
+              </div>
+              <button>Entrar</button>
+            </form>
+            <div class="text-right">
+              <a href="#">Criar uma conta</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+main {
+  background-color: var(--bg-blue);
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.login-area {
+  margin: 0 auto;
+  width: 400px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+form {
+  width: 400px;
+  background-color: var(--bg-light-blue);
+  color: #fff;
+  padding: 20px;
+  margin: 0 auto;
+  border-radius: 5px;
 }
-
-nav a {
+form button {
+  background-color: var(--lightness);
+  width: 100%;
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  border: none;
+  padding: 10px 15px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: var(--bg-blue);
+  border-radius: 5px;
+  margin-top: 20px;
 }
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.login-area a {
+  color: #fff;
+  display: inline-block;
+  margin-top: 10px;
 }
 </style>
